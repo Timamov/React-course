3c62c883bc71269f2d8b1ee82a31fcf38d30ba62
Timur Movchan <timurmov25@gmail.com>
2025-05-18 19:31:16 +0300

    Learned about using the javascript classic fundamentals for react

--- src/Button.jsx
+++ src/Button.jsx
@@ -3,7 +3,8 @@ export default function Button({  setNumber }) {
     return (
         <button onClick={() => {
            // setNumber(number + 1);
-            setNumber((prev) => prev + 1);
+            setNumber = (prev) => prev + 1;
+            return setNumber;
         }}>+</button>
     )
 }
